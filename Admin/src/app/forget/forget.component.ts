ec8b895d954f37df30a4d83ca154f4f3e16a4404
VaghelaKishan <vaghelakishan59@gmail.com>
2023-03-17 10:33:45 +0530

    update

--- Admin/src/app/forget/forget.component.ts
+++ Admin/src/app/forget/forget.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-forget',
   templateUrl: './forget.component.html',
   styleUrls: ['./forget.component.css']
 })
-export class ForgetComponent {
-
+export class ForgetComponent implements OnInit {
+  ngOnInit():void{
+    (function () {
+      'use strict'
+    
+      // Fetch all the forms we want to apply custom Bootstrap validation styles to
+      var forms = document.querySelectorAll('.needs-validation')
+    
+      // Loop over them and prevent submission
+      Array.prototype.slice.call(forms)
+        .forEach(function (form) {
+          form.addEventListener('submit', function (event: { preventDefault: () => void; stopPropagation: () => void; }) {
+            if (!form.checkValidity()) {
+              event.preventDefault()
+              event.stopPropagation()
+            }
+    
+            form.classList.add('was-validated')
+          }, false)
+        })
+    })()
+  }
 }
